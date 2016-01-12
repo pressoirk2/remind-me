866a3e6f09bd14a6c630d0ca9c65315da01b9f86
pressoirk2 <pressoirk2@owls.southernct.edu>
2016-01-12 18:09:00 -0500

    add event handlers for task buttons

--- client/remind-me.js
+++ client/remind-me.js
@@ -24,4 +24,17 @@
       event.target.text.value = "";
     }
   });
+
+//event handlers for task buttons
+  Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
 }
